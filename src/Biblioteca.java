c146b8b689d915bad495e6f69d395b3e3f9cc718
Thiago Vignolo Ferreira <thiagovignoloferreira@gmail.com>
2023-11-22 13:05:55 +0100

    pause.

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -4,13 +4,19 @@ public class Biblioteca {
     public static void main(String[] args) {
         boolean run = true;
         int opcion;
+
+        long cod; String titulo, autor;
+
         Scanner scanner = new Scanner(System.in);
+
+        //Declaracion de DataOutputStream y DataInputStream
 
         while (run){
 
             System.out.println("Escoja una opcion:" +
                     "\n1. Escribir un libro." +
-                    "\n2. Leer libros.");
+                    "\n2. Leer libros." +
+                    "\n3. Salir.");
 
             opcion = scanner.nextInt();
 
